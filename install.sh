3b3ce463772fdffaf046aa70fa7aaf2441ebad56
Maren Afflerbach <maren.afflerbach@sixt.com>
2019-03-29 11:26:03 +0100

    update linking

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+
+# i3 config
+ln -s ~/dotfiles/i3/config ~/.config/i3/config 
+ln -s ~/dotfiles/i3/compton.conf ~/.config/i3/compton.conf 
+ln -s ~/dotfiles/i3/rofi.rasi ~/.config/i3/rofi.radi 
+ln -s ~/dotfiles/i3/i3status.conf ~/.config/i3/i3status.conf 
+
+# vim 
+ln -s ~/dotfiles/vim/vimrc .vimrc
+
+# qutebrowser
+ln -s ~/dotfiles/qutebrowser/config.py ~/.qutebrowser/config.py 
+
+
+
