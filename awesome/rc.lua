a3c032809238e7a6cf2a5701496b685c0f26b540
Maren Afflerbach <maren@afflerbach.info>
2020-12-05 10:57:46 +0100

    backup commit

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -62,21 +62,21 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     --awful.layout.suit.floating,
+--     awful.layout.suit.corner.ne,
+ --    awful.layout.suit.corner.sw,
+  --   awful.layout.suit.corner.se,
+--    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    --awful.layout.suit.tile.top,
+    awful.layout.suit.corner.nw,
     awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal
+    --awful.layout.suit.tile.bottom,
+    --awful.layout.suit.magnifier
+    --awful.layout.suit.tile.left,
+    --awful.layout.suit.tile.top,
+    --awful.layout.suit.fair.horizontal
     --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
     --awful.layout.suit.max,
     --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier
-    --awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
 }
 -- }}}
 
@@ -169,8 +169,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-
+    awful.tag({ "Dev", "Web", "Com", "4", "5", "6" }, s, awful.layout.layouts[1])
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
@@ -197,8 +196,6 @@ awful.screen.connect_for_each_screen(function(s)
 
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", screen = s })
-    local battery_widget = require("awesome-wm-widgets.battery-widget.battery")
-    local cpu_widget = require("awesome-wm-widgets.cpu-widget.cpu-widget")
     -- Add widgets to the wibox
     s.mywibox:setup {
         layout = wibox.layout.align.horizontal,
@@ -513,11 +510,7 @@ awful.rules.rules = {
         class = {
           "Arandr",
           "Blueman-manager",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
           "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
           "xtightvncviewer"},
@@ -610,8 +603,6 @@ end)
 client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
-
-
 
 
 -- }}}
--- scripts/openProject.sh
+++ scripts/openProject.sh
@@ -1,10 +1,28 @@
 #!/bin/zsh
 
-FQSN=`ls -1 /home/maren/development | rofi -dmenu -theme ~/dotfiles/i3/rofi.rasi ` 
+#FQSN=`ls -1 /home/maren/development | rofi -dmenu -theme ~/dotfiles/i3/rofi.rasi ` 
 echo "export FQSN=$FQSN" > /home/maren/exportedVars 
 echo $FQSN | xsel --clipboard --input
 
-cd ~/development/$FQSN
-vim
+#FQSN=`ls -1 /home/maren/development/solutions.common.configuration/src | rofi -dmenu -theme ~/dotfiles/i3/rofi.rasi ` 
+
+devDir=$(ls -1 /home/maren/development)
+solutionsDir=$(ls -1 /home/maren/development/solutions.common.configuration/src)
+
+FQSN=$(echo -e $devDir"\n"$solutionsDir |  rofi -dmenu -theme ~/dotfiles/i3/rofi.rasi)
+
+if [ -d ~/development/$FQSN ]; then
+    cd ~/development/$FQSN
+    nvim
+else 
+    cd ~/development
+   checkoutName="solutions.java.$FQSN"
+    gh repo clone "Sixt-Int/$checkoutName"
+    if [ -d ~/development/$checkoutName ]; then
+        cd ~/development/$checkoutName
+        nvim
+    fi
+fi
+
 zsh
 
